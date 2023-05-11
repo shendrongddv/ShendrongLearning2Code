971641c06aaa514e4d01ab6229f3e0b071c3072d
shendrongddv <shendrongddv@gmail.com>
2023-05-11 16:39:59 +0700

    2023-05-23

---
+++ ca/20230506/script.js
@@ -0,0 +1,34 @@
+"use stritch";
+
+const result = document.querySelector(".result input");
+const copyBtn = document.getElementById("copy-btn");
+const sliders = document.querySelectorAll(".wrapper input[type='range']");
+const rColor = document.getElementById("red");
+const gColor = document.getElementById("green");
+const bColor = document.getElementById("blue");
+
+let generateColor = () => {
+  let rColorValue = rColor.value;
+  let gColorValue = gColor.value;
+  let bColorValue = bColor.value;
+
+  let finalColor = `rgb(${rColorValue}, ${gColorValue}, ${bColorValue})`;
+  result.value = finalColor;
+  document.body.style.backgroundColor = finalColor;
+};
+
+let copyColorCode = () => {
+  result.select();
+  document.execCommand("copy");
+  copyBtn.innerHTML = "Copied";
+  setTimeout(() => {
+    copyBtn.innerHTML = "Copy Color Code";
+  }, 1000);
+};
+
+sliders.forEach((inp) => {
+  inp.addEventListener("input", generateColor);
+});
+
+window.addEventListener("load", generateColor);
+copyBtn.addEventListener("click", copyColorCode);
