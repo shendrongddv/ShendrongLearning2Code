a54acbe48d57aebf1eda4011f963edc3b5966baf
shendrongddv <shendrongddv@gmail.com>
2023-05-26 13:13:28 +0700

    first commit

--- cn/20221004/script.js
+++ cn/20221004/script.js
@@ -9,7 +9,7 @@ const characters = {
   lowercase: "abcdefghijklmnopqrstuvwxyz",
   uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   numbers: "0123456789",
-  Symbol: "^!$%&|[](){}:;.,*+-#@<>~",
+  symbol: "^!$%&|[](){}:;.,*+-#@<>~",
 };
 
 const generatePassword = () => {
